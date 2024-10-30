d869090124cefc5ba72fca9f560ab00ab2cb6135
Valeriusdev <kakao757@gmail.com>
2024-10-29 18:24:54 -0700

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from 'react'
 import TaskForm from './components/Tasks/TaskForm'
 import TaskList from './components/Tasks/TaskList'
 import './App.css'
 
 const App = () => {
+  const [tasks, setTasks] = useState([])
+
   return (
     <div>
       <h1> Task Manager </h1>
       <TaskForm />
-      <TaskList />
+      <TaskList tasks={tasks}/>
     </div>
   )
 }
