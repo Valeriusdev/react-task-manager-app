0da8ac512de08e45b795deb949e9b3053f3f9f56
Valeriusdev <kakao757@gmail.com>
2024-10-30 20:11:07 -0700

    update TaskForm component

--- src/components/Tasks/TaskForm.jsx
+++ src/components/Tasks/TaskForm.jsx
@@ -1,7 +1,20 @@
-const TaskForm = () => {
+import { useState } from "react"
+
+const TaskForm = ({ addTask }) => {
+  const[text, setText] = useState('')
+  const onSubmitHandler = (event) => {
+    event.preventDefault()
+    addTask(text)
+    setText('')
+  } 
+
   return (
-    <form>
-        <input placeholder="Enter new task" />
+    <form onSubmit={onSubmitHandler}>
+        <input 
+          placeholder="Enter new task"
+          value={text}
+          onChange={(e) => setText(e.target.value)} 
+        />
         <button type="submit"> Submit </button>
     </form>
   )
