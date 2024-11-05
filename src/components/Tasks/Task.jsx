18f21f0713217ef66172064af40c2b9f8355f3f4
Valeriusdev <kakao757@gmail.com>
2024-11-04 20:45:46 -0800

    update Task component

--- src/components/Tasks/Task.jsx
+++ src/components/Tasks/Task.jsx
@@ -1,8 +1,10 @@
 import styles from './Task.module.css'
+import { RiTodoFill } from 'react-icons/ri'
 
 const Task = ({ task, index, deleteTask }) => {
   return (
     <div className={styles.task} onDoubleClick={() => deleteTask(index)}>
+      <RiTodoFill />
       <div className={styles.taskText}>{task}</div>
     </div>
   )
