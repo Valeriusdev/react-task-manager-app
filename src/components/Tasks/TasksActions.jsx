c025086e1a7f6bdf2aa7ab1800ca809919ad534c
Valeriusdev <kakao757@gmail.com>
2024-12-12 20:48:47 -0800

    update TasksActions component

--- src/components/Tasks/TasksActions.jsx
+++ src/components/Tasks/TasksActions.jsx
@@ -1,13 +1,21 @@
 import { RiRefreshLine, RiDeleteBin2Line } from "react-icons/ri";
 import Button from "../../UI/Button";
 
-const TasksActions = ({ resetTasks, deleteCompletedTasks, completedTasksExist }) => {
+const TasksActions = ({
+  resetTasks,
+  deleteCompletedTasks,
+  completedTasksExist,
+}) => {
   return (
     <>
       <Button title="Reset Tasks" onClick={resetTasks}>
         <RiRefreshLine />
       </Button>
-      <Button title="Clear Completed Tasks" onClick={deleteCompletedTasks} disabled={!completedTasksExist} >
+      <Button
+        title="Clear Completed Tasks"
+        onClick={deleteCompletedTasks}
+        disabled={!completedTasksExist}
+      >
         <RiDeleteBin2Line />
       </Button>
     </>
